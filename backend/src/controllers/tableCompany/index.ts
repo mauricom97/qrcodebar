71103e31cb22067556cdf83c9a932d5307955f43
Mauricio Nunes <mauricionunesdasilvanunes@gmail.com>
2023-05-04 20:17:33 -0300

    adicionando authenticacao no index das mesas

--- backend/src/controllers/tableCompany/index.ts
+++ backend/src/controllers/tableCompany/index.ts
@@ -1,9 +1,9 @@
 import TableCompany from '../../db/models/TableCompany';
 import { Request, Response } from 'express';
 
-export const index = async (req: Request, res: Response) => {
+export const index = async (req: any, res: Response) => {
     try {
-        const table_company = await TableCompany.findAll()
+        const table_company = await TableCompany.schema(req.company.schemaName).findAll()
         return res.send(table_company)
     } catch (error) {
         return res.status(500).json({ error });
