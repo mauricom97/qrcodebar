a1313c38ccb77a663a12018e00acaeea338e597b
Mauricio Nunes <mauricionunesdasilvanunes@gmail.com>
2023-06-18 00:14:12 -0300

    update

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,5 +1,10 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['<rootDir>/src/__tests__/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/__tests__/**/*.ts'
+  ]
 };
