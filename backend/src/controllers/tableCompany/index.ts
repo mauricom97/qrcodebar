207ec4c71b809d6d11d8f9257eccf48a7254ab30
Mauricio Nunes <mauricionunesdasilvanunes@gmail.com>
2023-07-05 18:50:14 -0300

    update

--- backend/src/controllers/tableCompany/index.ts
+++ backend/src/controllers/tableCompany/index.ts
@@ -1,9 +1,29 @@
 import TableCompany from '../../db/models/TableCompany';
+import QRCode from '../../db/models/QrCode';
 import { Response } from 'express';
+import _ from 'lodash';
 
 export const index = async (req: any, res: Response) => {
     try {
-        const table_company = await TableCompany.schema(req.company.schemaName).findAll()
+        let table_company: any = await TableCompany.findAll({
+            raw: true,
+            where: {
+                company_uuid: req.company.uuid
+            }
+        })
+        table_company = _.orderBy(table_company, ['num'], ['asc'])
+        for(let table of table_company){
+            let qrcode: any = await QRCode.findOne({
+                attributes: ['image'],
+                raw: true,
+                where: {
+                    uuid_company_table: table.uuid,
+                    company_uuid: req.company.uuid
+                }
+            })
+            table.qrcode = qrcode.image
+        }
+        console.log(table_company)
         return res.send(table_company)
     } catch (error) {
         return res.status(500).json({ error });
